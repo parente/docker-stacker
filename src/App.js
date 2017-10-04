bfb5ff05c99d5bd86fa22d587dea5ad3047d2ac8
Peter Parente <parente@cs.unc.edu>
2017-10-04 00:22:06 -0400

    new virtual selectize, layout, julia data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React, { Component } from 'react';
-import {ReactSelectize, SimpleSelect, MultiSelect} from 'react-selectize';
+import VirtualizedSelect from 'react-virtualized-select'
+import julia from './julia';
+
 import './App.css';
-import 'react-selectize/themes/index.css';
+import 'react-select/dist/react-select.css'
+import 'react-virtualized/styles.css'
+import 'react-virtualized-select/styles.css'
 
 const Card = ({ columns, header, title, children }) => (
-  <div className={`col-${columns}`}>
-    <div className="card">
-      <div className="card-header text-muted">
-        {header}
-      </div>
-      <div className="card-body">
-        <h4 className="card-title">{title}</h4>
-        {children}
-      </div>
+  <div className="card">
+    <div className="card-header text-muted">
+      {header}
+    </div>
+    <div className="card-body">
+      <h4 className="card-title">{title}</h4>
+      {children}
     </div>
   </div>
 );
 
-const PDFSupport = ({enablePdf, handleInputChange}) => {
+const PDFSupport = ({enablePdf, onChange}) => {
   return (
     <div className="card-text">
       <label className="form-check-label">
@@ -25,8 +27,8 @@ const PDFSupport = ({enablePdf, handleInputChange}) => {
           type="checkbox"
           name="enablePdf"
           checked={enablePdf}
-          onChange={handleInputChange} />
-        <span>Support conversion of notebooks to LaTeX and PDF files by installing Pandoc and texlive</span>
+          onChange={onChange} />
+        <span>Install Pandoc and texlive to support conversion of notebooks to LaTeX and PDF files</span>
       </label>
     </div>
   );
@@ -34,15 +36,15 @@ const PDFSupport = ({enablePdf, handleInputChange}) => {
 
 const TextEditors = (props) => (
   <div className="card-text">
-    <p>Add text editors editors for use in the terminal</p>
+    <p>Install text editors for use in the terminal</p>
     <div className="form-check">
       <label className="form-check-label">
         <input className="form-check-input"
           type="checkbox"
           name="enableEmacs"
           checked={props.enableEmacs}
-          onChange={props.handleInputChange} />
-        <span>emacs</span>
+          onChange={props.onChange} />
+        <span>Emacs</span>
       </label>
     </div>
     <div className="form-check">
@@ -51,8 +53,8 @@ const TextEditors = (props) => (
           type="checkbox"
           name="enableJed"
           checked={props.enableJed}
-          onChange={props.handleInputChange} />
-        <span>jed</span>
+          onChange={props.onChange} />
+        <span>JED</span>
       </label>
     </div>
     <div className="form-check">
@@ -61,8 +63,8 @@ const TextEditors = (props) => (
           type="checkbox"
           name="enableVim"
           checked={props.enableVim}
-          onChange={props.handleInputChange} />
-        <span>vim</span>
+          onChange={props.onChange} />
+        <span>Vim</span>
       </label>
     </div>
   </div>
@@ -70,22 +72,53 @@ const TextEditors = (props) => (
 
 const PackageSupport = (props) => (
   <div className="card-text">
-    <p>Packages to install for use in notebooks</p>
+    <p>{props.description}</p>
+    <VirtualizedSelect
+      name={props.name}
+      placeholder="Select packages"
+      multi={true}
+      searchable={true}
+      clearable={true}
+      options={props.options}
+      onChange={(item) => {
+        props.onChange({target: {name: props.name, value: item}})
+      }}
+      value={props.selected}
+    />
   </div>
 );
+
+const SparkSupport = ({ enableSpark, handleInputChange }) => {
+  return (
+    <div className="card-text">
+      <label className="form-check-label">
+        <input className="form-check-input"
+          type="checkbox"
+          name="enableSpark"
+          checked={enableSpark}
+          onChange={handleInputChange} />
+        <span>Install Apache Spark 2.2.0 for Hadoop 2.7 or higher</span>
+      </label>
+    </div>
+  );
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      availableJuliaPackages: julia.packages
+    };
     this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   handleInputChange(event) {
+    console.log(event);
     const target = event.target;
-    const name = event.target.name;
+    const name = target.name;
     const value = target.type === 'checkbox' ? target.checked : target.value;
 
+    console.log('setting state:', name, value)
     this.setState({
       [name]: value
     });
@@ -94,15 +127,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header container">
+        <div className="App-header sticky-top container">
           <nav className="navbar navbar-expand-lg navbar-light">
             <a className="navbar-brand" href="#">
               <img className="App-logo" src="https://cdn.jupyter.org/notebook/5.0.0/base/images/logo.png" alt="Jupyter" />
             </a>
           </nav>
         </div>
-
-        <div className="App-fullbar"></div>
 
         <div className="App-intro container">
           <div className="jumbotron">
@@ -114,76 +145,69 @@ class App extends Component {
         </div>
 
         <div className="App-controls container">
-          <div className="row">
-            <div className="col-12">
-            <MultiSelect
-              placeholder="Select a fruit"
-              onValueChange={value => alert(value)}>
-              <option value = "apple">apple</option>
-              <option value = "mango">mango</option>
-              <option value = "orange">orange</option>
-              <option value = "banana">banana</option>
-            </MultiSelect>
-            </div>
-          </div>
-
-          <div className="row">
+          <div className="card-deck">
             <Card
-              columns="12"
               header="Libraries"
               title="Python Environment">
               <PackageSupport
-                language="python"
-                packages={this.state.pythonPackages}
+                description="Install Python packages from https://conda-forge.github.io/feedstocks. The IPython kernel is always installed for you."
+                name="python"
+                options={this.state.pythonPackages}
                 onChange={this.handleInputChange} />
             </Card>
-          </div>
 
-          <div className="row">
             <Card
-              columns="12"
               header="Libraries"
               title="R Environment">
               <PackageSupport
-                language="r"
-                enableLanguage={this.state.enableR}
-                packages={this.state.rPackages}
+                description="Install R packages from https://conda-forge.github.io/feedstocks. The IRKernel package will be installed for you if you choose at least one other package."
+                name="r"
+                options={this.state.rPackages}
                 onChange={this.handleInputChange} />
             </Card>
           </div>
 
-          <div className="row">
+          <div className="card-deck">
             <Card
-              columns="12"
               header="Libraries"
               title="Julia Environment">
               <PackageSupport
-                language="julia"
-                enableLanguage={this.state.enableJulia}
-                packages={this.state.juliaPackages}
+                description="Install Julia packages from https://pkg.julialang.org/. The IJulia package will be installed for you if you choose at least one other package."
+                name="juliaPackages"
+                selected={this.state.juliaPackages}
+                options={this.state.availableJuliaPackages}
+                onChange={this.handleInputChange} />
+            </Card>
+
+            <Card
+              header="Libraries"
+              title="Scala Environment">
+              <PackageSupport
+                description="Install Scala kernels. Apache Spark will be installed for you if you choose at least one kernel."
+                name="scala"
+                packages={this.state.scalaPackages}
                 onChange={this.handleInputChange} />
             </Card>
           </div>
 
-          <div className="row">
+          <div className="card-deck">
             <Card
-              columns="12"
               header="Libraries"
-              title="Scala Environment">
+              title="Apache Spark">
+              <SparkSupport
+                enableSpark={this.state.enableSpark}
+                onChange={this.handleInputChange} />
             </Card>
-          </div>
 
-          <div className="row">
             <Card
-              columns="6"
               header="Jupyter Notebook"
               title="LaTeX and PDF Conversion">
               <PDFSupport
                 enablePdf={this.state.enablePdf}
                 onChange={this.handleInputChange} />
             </Card>
+
             <Card
-              columns="6"
               header="Terminal"
               title="Text Editors">
               <TextEditors
@@ -191,14 +215,6 @@ class App extends Component {
                 enableJed={this.state.enableJed}
                 enableVim={this.state.enableVim}
                 onChange={this.handleInputChange} />
-            </Card>
-          </div>
-
-          <div className="row">
-            <Card
-              columns="6"
-              header="Libraries"
-              title="Apache Spark">
             </Card>
           </div>
         </div>
